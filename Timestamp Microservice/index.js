b8afec06ec20cdc8590a2942e39d04f1e9c1c7e7
Gianpaolo Cuticchia <gianpaolojcc@gmail.com>
2024-05-05 12:29:30 +0200

    feat: manages unix, string and null dates. Resolution of the project

--- Timestamp Microservice/index.js
+++ Timestamp Microservice/index.js
@@ -24,7 +24,36 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// timestamp API
+datePath = '/api/:date?';
+app.get(datePath, (req, res) => {
+  let { date } = req.params;
+  
+  // check if no parameter was passed
+  if (!date) {
+    date = Date.now(); // in that case, assign the date as right now
+  }
 
+  let timestamp;
+  let utc;
+
+  // check if the provided date is a valid unix timestamp
+  if (!isNaN(date)) {
+    timestamp = parseInt(date); // convert date string to a number
+    utc = new Date(timestamp).toUTCString(); // convert number to utc date format
+  } else {
+    // otherwise treat it as a date string
+    timestamp = Date.parse(date); // convert date string to timestamp in milliseconds
+    utc = new Date(date).toUTCString(); // convert date to utc date format
+  }
+
+  // check if the provided date is valid
+  if (!isNaN(timestamp)) {
+    res.json({ unix: timestamp, utc });
+  } else {
+    res.json({ error: "Invalid date" });
+  }
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
