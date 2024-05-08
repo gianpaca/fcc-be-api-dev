5b30c2e9c614b07de2c5cf39d4a5b4463b56d730
Gianpaolo Cuticchia <gianpaolojcc@gmail.com>
2024-05-08 22:53:15 +0200

    feat: completes the project as requested

--- URL Shortener Microservice/index.js
+++ URL Shortener Microservice/index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+// include middleware to parse post requests
+const bodyParser = require("body-parser");
+// include valid-url for url validation
+const validUrl = require('valid-url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,6 +14,9 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -17,6 +24,46 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+const shorturl = '/api/shorturl';
+// database dictionary to store created shorturls
+const urlDb = {};
+
+// var to act as the shortened url
+let shorturlId = 0;
+
+app.post(shorturl, (req, res) => {
+  const originalUrl = req.body.url;
+   
+  // validate the URL format
+  if (!validUrl.isUri(originalUrl)) {
+    return res.status(400).json({ error: 'invalid url' });
+  }
+
+  // add one to the short-id value and assign it as the short-url
+  const shortUrl = ++shorturlId;
+
+  // store the URL mapping
+  urlDb[shortUrl] = originalUrl;
+
+  // send JSON response with original and short URLs
+  res.json({ original_url: originalUrl, short_url: shortUrl });
+});
+
+app.get(shorturl+'/:short_url', (req, res) => {
+  const shortUrl = req.params.short_url;
+
+  // lookup the original URL from the database
+  const originalUrl = urlDb[shortUrl];
+
+  // If not found, return an error
+  if (!originalUrl) {
+    return res.status(404).json({ error: 'short url not found' });
+  }
+
+  // otherwise, redirect to the original URL
+  res.redirect(originalUrl);
 });
 
 app.listen(port, function() {
