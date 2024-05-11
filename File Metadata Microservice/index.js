048a5c2e191522f97fdaf3520688bcab96e6d09b
Gianpaolo Cuticchia <gianpaolojcc@gmail.com>
2024-05-11 13:10:22 +0200

    feat: completed user stories for the project

--- File Metadata Microservice/index.js
+++ File Metadata Microservice/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
 
 var app = express();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +15,19 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const fileUploadPath = '/api/fileanalyse';
 
+app.post(fileUploadPath, upload.single('upfile'), (req, res) => {
+  if(!req.file) {
+    res.status(400).json({ error: 'No file selected for upload' });
+  }
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
